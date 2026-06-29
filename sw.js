8a2508f8e7c0a35aeba31faf63ad88f3cb740f0f
Berk Asatekin <52719295+Skrillalien@users.noreply.github.com>
2026-06-29 16:07:35 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,19 @@
-const CACHE = 'dilara-app-v1';
-const ASSETS = ['/', '/index.html'];
-
+// Cache temizle, eski SW'yi kaldır
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/index.html')))
-  );
+  // Cache kullanma, her zaman network'ten al
+  e.respondWith(fetch(e.request));
 });
 
 self.addEventListener('notificationclick', e => {
